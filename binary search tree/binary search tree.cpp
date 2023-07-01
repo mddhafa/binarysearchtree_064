d685e78290422e381e66ed40bfbf555bc0b616b6
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-07-01 21:43:45 +0700

    menambahkan exit dan enter your choice

--- binary search tree/binary search tree.cpp
+++ binary search tree/binary search tree.cpp
@@ -121,6 +121,7 @@ int main()
 		cout << "2. perform inorder traversal" << endl;
 		cout << "3. perform preorder traversal" << endl;
 		cout << "4. perform postorder traversal" << endl;
-		
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) :";
 	}
 }
