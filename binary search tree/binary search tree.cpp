8ba41e6ff1c5f01d8b1a7652dd6e5d20784cb3b4
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-07-01 20:48:44 +0700

    Add project files.

---
+++ binary search tree/binary search tree.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Node {
+public :
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	//Constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
+
